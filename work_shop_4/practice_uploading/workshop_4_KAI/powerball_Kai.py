7ad000c782b5d1361c71f7e0fa738c5421fbd5b0
huansongzeng <winstonzeng@canva.com>
2024-11-03 20:00:42 +1100

    update naming space

--- work_shop_4/practice_uploading/workshop_4_KAI/powerball_Kai.py
+++ work_shop_4/practice_uploading/workshop_4_KAI/powerball_Kai.py
@@ -1,10 +1,10 @@
 import random
 
-def picknumbers(poolsize, ballnum):
-    return random.sample(range(1, poolsize + 1), ballnum)
+def pick_numbers(pool_size, ball_num):
+    return random.sample(range(1, pool_size + 1), ball_num)
 
-winning_numbers = picknumbers(35, 7)
-power_ball = picknumbers(20, 1) 
+winning_numbers = pick_numbers(35, 7)
+power_ball = pick_numbers(20, 1) 
 
 print(f'the winning numbers are: {winning_numbers}')
 print(f'the power ball is {power_ball}')
--- work_shop_4/practice_uploading/workshop_4_shawn/powerball_shawn.py
+++ work_shop_4/practice_uploading/workshop_4_shawn/powerball_shawn.py
@@ -1,22 +1,22 @@
 import random
 
-def picknumbers(poolsize, ballnum):
+def pick_numbers(pool_size, ball_num):
     #Create a list of numbers from 1 to defined poolsize
-    numbers = list(range(1, poolsize + 1))
+    numbers = list(range(1, pool_size + 1))
     #Shuffle
     random.shuffle(numbers)
-    # return numbers[-ballnum:]
+    # return numbers[-ball_num:]
     print(numbers)
     #Initialize winning list
     winning_numbers = []
     #set a condition to pop the number from shuffled number list and append to winning_numbers
-    while len(winning_numbers) < ballnum:
+    while len(winning_numbers) < ball_num:
         winning_numbers.append(numbers.pop())
 
     return winning_numbers
 
-winning_seven = picknumbers(35, 7)
-p_ball = picknumbers(20,1)
+winning_seven = pick_numbers(35, 7)
+p_ball = pick_numbers(20,1)
 
 print(f'the winning seven numbers are: {winning_seven}')
 print(f'the P ball is {p_ball}')
