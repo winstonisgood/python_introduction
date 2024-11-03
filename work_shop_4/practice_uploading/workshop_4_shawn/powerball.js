4925c1d85c77785c21cdd00b96c3a1a2ee275702
huansongzeng <winstonzeng@canva.com>
2024-11-03 20:25:57 +1100

    add the local function

--- work_shop_4/practice_uploading/workshop_4_shawn/powerball.js
+++ work_shop_4/practice_uploading/workshop_4_shawn/powerball.js
@@ -1,17 +1,21 @@
 function pickNumbers(poolSize, ballNum) {
+    function shuffle(numbers, poolSize) {
+        for (let i = 1; i <= poolSize; i++) {
+            numbers.push(i);
+        }
+
+        // Random index and shuffle numbers (Fisher-Yates algorithm)
+        for (let i = numbers.length - 1; i > 0; i--) {
+            // Pick random index from 0 to i inclusive
+            const j = Math.floor(Math.random() * (i + 1));
+            // Swap numbers[i] and numbers[j] (at random index)
+            [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+        }
+    }
+
     // Create and fill numbers array
     const numbers = [];
-    for (let i = 1; i <= poolSize; i++) {
-        numbers.push(i);
-    }
-    
-    // Random index and shuffle numbers (Fisher-Yates algorithm)
-    for (let i = numbers.length - 1; i > 0; i--) {
-        // Pick random index from 0 to i inclusive
-        const j = Math.floor(Math.random() * (i + 1));
-        // Swap numbers[i] and numbers[j] (at random index)
-        [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
-    }
+    shuffle(numbers, poolSize);
     
     // Initialize the winning numbers array
     const winningNumbers = [];
