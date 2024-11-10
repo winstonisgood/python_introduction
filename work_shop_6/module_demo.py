d6cd5bcba900ec2e1f92991a185db8eb60223cbc
huansongzeng <winstonzeng@canva.com>
2024-11-10 19:42:59 +1100

    module demo

---
+++ work_shop_6/module_demo.py
@@ -0,0 +1,5 @@
+import my_module as mm
+
+print(mm.greeting("Jonathan"))  # Hello, Jonathan!
+print(mm.person)  # {'name': 'Jonathan', 'age': 30, 'country': 'USA'}
+print(mm.person["name"])  # Jonathan
---
+++ work_shop_6/my_module.py
@@ -0,0 +1,8 @@
+def greeting(name):
+    return f"Hello, {name}!"
+
+person = {
+    "name": "Jonathan",
+    "age": 30,
+    "country": "USA"
+}
